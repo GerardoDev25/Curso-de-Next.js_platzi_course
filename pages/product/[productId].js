ee34f48439999928e82040f78f7247a9a7f52012
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-02 09:42:19 -0400

    #UnderTheHood setup y páginas: optimizaciones ocultas

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 const ProdictId = () => {
   const { query } = useRouter();
 
-  return <div>ProdictId: {query.id}</div>;
+  return <div>ProdictId: {query.productId}</div>;
 };
 
 export default ProdictId;
