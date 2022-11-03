611cd98fc3d8c331c89f20a8bf067317dd35e77b
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-03 09:20:25 -0400

    Path alias

--- components/Layaut/Layaut.tsx
+++ components/Layaut/Layaut.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+// import Navbar from '../Navbar/Navbar'
+import Navbar from '@components/Navbar/Navbar'
 
 const Layaut: React.FC = ({ children }) => {
   return (
