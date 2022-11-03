bc1aff21055ce0e3c9d96378915b5cf18529c91c
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-03 09:37:47 -0400

    Explora las soluciones de CSS en NextJS y su flexibilidad

--- components/Layaut/Layaut.tsx
+++ components/Layaut/Layaut.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 // import Navbar from '../Navbar/Navbar'
 import Navbar from '@components/Navbar/Navbar'
+// import style from './layout.module.css'
 
 const Layaut: React.FC = ({ children }) => {
   return (
     <div>
       <Navbar />
       {children}
-      <footer>This is the footer</footer>
+      <footer className="container">This is the footer</footer>
+      <style jsx>{`
+        .container {
+          color: red;
+          font-size: 4rem;
+        }
+
+        div {
+          background-color: salmon;
+        }
+      `}</style>
     </div>
   )
 }
